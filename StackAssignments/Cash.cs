c3812405ffd5e5b8d4e86bc25afc6cb54a726e11
laxmankumar2000 <laxmankumarvashisth@gmail.com>
2024-06-25 15:28:52 +0530

    Add project files.

---
+++ StackAssignments/Cash.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAssignments
+{
+    public class Cash
+    {
+        public double getTotalCost()
+        {
+            double totalCost = 0;
+
+            foreach(var item in Program.st)
+            {
+                totalCost += item;
+            }    
+            return totalCost;
+        }
+
+        public double getMaxVal()
+        {
+            double maxVal = -1.0;
+            foreach(var item in Program.st)
+            {
+                if (maxVal <item)
+                    maxVal = item;
+            }
+            return maxVal;
+
+        }
+
+        public double getMinVal()
+        {
+            double minVal = 100000;
+            foreach(var item in Program.st)
+            {
+                if(minVal>item)
+                    minVal = item;
+            }
+            return minVal;
+        }
+    }
+}
---
+++ StackAssignments/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAssignments
+{
+    public class Program
+    {
+        public static Stack<double> st = new Stack<double>();
+        public static void  Main()
+        {
+
+            Console.WriteLine("1. Find the total amount");
+            Console.WriteLine("2. Find the max cash");
+            Console.WriteLine("3. Find the min Cahs");
+            Cash c = new Cash();
+            st.Push(12.22);
+            st.Push(23.33);
+            st.Push(223.23);
+
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("Enter the choice");
+                choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        {
+
+                            Console.WriteLine(c.getTotalCost());
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Max value" + c.getMaxVal());
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Min Value" + c.getMinVal());
+                            break;
+                        }
+                }
+            } while (choice != 4);
+                 
+           
+        }
+    }
+}
